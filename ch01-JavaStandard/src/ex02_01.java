87b81ef1c44817995ad950cc913458cbdad44f36
Yunhyunjong <yyunhjoo@naver.com>
2024-01-14 22:18:09 +0900

    JavaStandard commit test

---
+++ ch01-JavaStandard/src/ex02_01.java
@@ -0,0 +1,11 @@
+
+public class ex02_01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Hyunjong");
+		
+		System.out.println("3+5");
+		System.out.println(3+5);
+	}
+
+}
